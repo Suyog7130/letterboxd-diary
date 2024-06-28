82b5024b846e5c9f848481fe19e6355e7952e8c7
Suyog Garg <suyog999sg@gmail.com>
2024-06-28 15:37:50 +0900

    make list horizontal, only movie posters, add old format as diary-full.ts

--- views/diary-full.ts
+++ views/diary-full.ts
@@ -16,23 +16,23 @@ export default function diaryView(diaryList: Diary[], username: string): string 
 <style>
     #letterboxd-embed-tc {
         display: flex;
-        flex-direction: row;
-        gap: 0.5rem;
+        flex-direction: column;
+        gap: 1rem;
         width: 100%;
-        font-size: 0.5rem;
+        font-size: 1rem;
     }
 
     .letterboxd-embed-tc-diary-entry {
         display: flex;
-        flex-direction: row;
-        gap: 0.5rem;
-        padding: 0 .25rem;
+        flex-direction: column;
+        gap: 1rem;
+        padding: 0 .5rem;
     }
 
     .letterboxd-embed-tc-title {
         font-weight: bold;
         font-size: 1.1rem;
-        margin-bottom: 0.05rem;
+        margin-bottom: 0.1rem;
         text-align: left;
         line-height: normal;
     }
@@ -98,13 +98,40 @@ export default function diaryView(diaryList: Diary[], username: string): string 
             <div class="letterboxd-embed-tc-content">
                 <div class="letterboxd-embed-tc-poster">
                     <a href="${diary.uri}" target="_blank">
-                        <img src="${diary.film?.image?.medium ?? ''}" alt="${diary.film.title} poster">
+                        <img src="${diary.film?.image?.small ?? ''}" alt="${diary.film.title} poster">
                     </a>
                 </div>
+
+                <div>
+                    <div class="letterboxd-embed-tc-title">
+                        ${diary.film.title}
+                        <span class="letterboxd-embed-tc-year">
+                            ${diary.film.year}
+                        </span>
+                    </div>
+
+                    <div class="letterboxd-embed-tc-date">
+                        ${diary.isRewatch ? '&#9850;' : ''} ${dateString}
+                    </div>
+
+                    <div class="letterboxd-embed-tc-rating">
+                        ${diary.rating.text ?? ''}
+                    </div>
+
+                    ${review ? `
+                         <div class="letterboxd-embed-tc-review">${review}</div>
+                     ` : ''}
+                </div>
+                
             </div>
-        </div>   
+        </div>
+
+        <div class="letterboxd-embed-tc-divider"></div>
     `}).join('')}
 
+    <div class="letterboxd-embed-tc-more">
+        <a href="https://letterboxd.com/${username}" target="_blank">...more on Letterboxd</a>
+    </div>
 </div>
 `;
 }
